485c9bb1d08caf6b121a270e42530403a22e2652
Mohammed27800 <asmte2780@gmail.com>
2024-11-06 14:44:32 +0300

    NavigationBar

---
+++ src/MVC/Views/Tabs/Analytics.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Analytics() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+    <Text className="text-lg font-bold text-black">Analytics Screen</Text>
+  </View>
+  )
+}
